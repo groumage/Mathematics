7dfaa6ce394115d54cd24467b17f4256ca5ba9a4
guillaume <guillaume.roumage@etu.u-pec.com>
2018-08-19 16:01:50 +0200

    html bien avance

--- html.ml
+++ html.ml
@@ -1,12 +1,13 @@
 type attribut = Alg of string
 							| Href of string
 							
-
+							
 type attributs = attribut list
 
 type style = Color of string
 					 | FontSize of int
 					 | BackgroundColor of string
+	
 
 type balise = Html
 						| Title
@@ -39,9 +40,15 @@ module M = Map.Make(B)
 type css = attributs M.t
 
 let m = M.empty
-let m = M.add (H 1) (Color "blue") m
-let m = M.add (H 2) ([Color "navy"; BackgroundColor "yellow"])
-let m = M.add (Li) (FontSize 10)
+let m = M.add (H 1) ([Color "blue"]) m
+let m = M.add (H 2) ([Color "navy"; BackgroundColor "yellow"]) m
+let m = M.add (Li) ([FontSize 10]) m
+
+let style_to_string2 s =
+	match s with
+	  Color s -> "color : " ^ s ^ ";"
+	| FontSize i -> "font-size : " ^ string_of_int i ^ "px;"
+	| BackgroundColor s -> "background-color : " ^ s ^ ";"
 
 let rec tree_to_string tree =
 	match tree with
@@ -52,14 +59,14 @@ let rec tree_to_string tree =
 												 | Head -> "<head " ^ attributes_to_string a ^ ">\n" ^ tree_to_list t ^ "</head>\n"
 												 | Body -> "<body " ^ attributes_to_string a ^ ">\n" ^ tree_to_list t ^ "</body>\n"
 												 | Link -> "<link " ^ attributes_to_string a ^ ">\n" ^ tree_to_list t ^ "</link>\n"
-												 | H i -> "<h" ^ string_of_int i ^ " " ^ attributes_to_string a ^ " style= \"" ^ style_to_string (H i) "\">" ^ tree_to_list t ^ "</h" ^ string_of_int i ^ ">\n"
+												 | H i -> "<h" ^ string_of_int i ^ " " ^ attributes_to_string a ^ "style = \"" ^ style_to_string (H i) ^ "\">" ^ tree_to_list t ^ "</h" ^ string_of_int i ^ ">\n"
 												 | B -> "<b " ^ attributes_to_string a ^ ">\n" ^ tree_to_list t ^ "</b>\n"
 												 | I -> "<i " ^ attributes_to_string a ^ ">\n" ^ tree_to_list t ^ "</i>\n"
 												 | U -> "<u " ^ attributes_to_string a ^ ">\n" ^ tree_to_list t ^ "</u>\n"
 												 | Del -> "<del " ^ attributes_to_string a ^ ">\n" ^ tree_to_list t ^ "</del>\n"
 												 | Ul -> "<ul " ^ attributes_to_string a ^ ">\n" ^ tree_to_list t ^ "</ul>\n"
-												 | Li -> "<li " ^ attributes_to_string a ^ ">\n" ^ tree_to_list t ^ "</li>\n"
 												 | P -> "<p " ^ attributes_to_string a ^ ">\n" ^ tree_to_list t ^ "</p>\n"
+												 | Li -> "<li " ^ attributes_to_string a ^ "style = \"" ^ style_to_string Li ^ "\">" ^ tree_to_list t ^ "</li>\n"
 												 | Empty -> attributes_to_string a ^ tree_to_list t
 												)
 and tree_to_list tree_list =
@@ -70,9 +77,16 @@ and attributes_to_string lst =
 	match lst with
 	  [] -> ""
 	| h :: t -> match h with
-								Alg s -> "align=\"" ^ s ^ "\" " ^ attributes_to_string t
-							| Href s -> "href=" ^ s ^ " " ^ attributes_to_string t
-and style_to_string
+								Alg s -> "align = \"" ^ s ^ "\" " ^ attributes_to_string t
+							| Href s -> "href = \"" ^ s ^ "\"" ^ attributes_to_string t
+and style_to_string balise =
+	let a = M.find balise m in
+	let rec decompose_lst lst =
+		match lst with
+			[] -> ""
+		| h :: t -> style_to_string2 h ^ " " ^ decompose_lst t
+	in
+	decompose_lst a
 
 let init_arbre =
 	let fic = open_out "res.html" in
